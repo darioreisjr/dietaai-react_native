05b964530093bc683d34972c23acc324f9cadaa5
Dario Reis <guttyerresjunior1995@gmail.com>
2024-09-21 08:29:57 -0300

    feat(home): create home page for the app
    Implemented the layout and basic structure of the home page, including navigation and initial styling.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,60 @@
-import { Text, View } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "../constants/colors";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>teste algo</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo.png")} />
+
+      <Text style={styles.title}>
+        Dieta<Text style={{ color: colors.white }}>.IA</Text>{" "}
+      </Text>
+
+      <Text style={styles.text}>
+        Sua dieta personalizada com inteligência artificial 🍴
+      </Text>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar dieta</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: colors.green,
+  },
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    width: 240,
+    textAlign: "center",
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: colors.blue,
+    width: "100%",
+    height: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 34,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: colors.white,
+    fontWeight: "bold",
+  },
+});
